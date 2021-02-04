90155e0c724a85310624e398380a3066f6edc8d7
Danilo Marques de Rezende <danilomr12@gmail.com>
2021-02-04 19:05:11 -0300

    msg: Initializing project with Grails 4.0.6 and plugins/configurations for rest api.

---
+++ grails-app/controllers/kote2/UrlMappings.groovy
@@ -0,0 +1,30 @@
+package kote2
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+
+/** REST VERSION OF MAPPING
+ *  static mappings = {
+        delete "/$controller/$id(.$format)?"(action:"delete")
+        get "/$controller(.$format)?"(action:"index")
+        get "/$controller/$id(.$format)?"(action:"show")
+        post "/$controller(.$format)?"(action:"save")
+        put "/$controller/$id(.$format)?"(action:"update")
+        patch "/$controller/$id(.$format)?"(action:"patch")
+
+        "/"(controller: 'application', action:'index')
+        "500"(view: '/error')
+        "404"(view: '/notFound')
+    }*/
+}
---
+++ grails-app/init/kote2/BootStrap.groovy
@@ -0,0 +1,9 @@
+package kote2
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
